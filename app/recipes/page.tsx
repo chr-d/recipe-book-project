05bbf3504faeb726ba713a0c251a298b0e3aa77b
chr-d <275697199+chr-d@users.noreply.github.com>
2026-09-15 13:57:04 +0200

    add recipe overview page and remove temporary search page

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -1,3 +1,25 @@
-export default function Recipes() {
-  return <h1>Recipes List</h1>;
+import { RecipeGrid } from "@/components/RecipeGrid";
+import { RecipeGridLoading } from "@/components/RecipeGridLoading";
+import { SearchBar } from "@/components/SearchBar";
+import { getAllRecipes, searchRecipes } from "@/db/queries";
+import { Suspense } from "react";
+
+export default async function Recipes({
+  searchParams,
+}: {
+  searchParams: Promise<{ q?: string }>;
+}) {
+  const params = await searchParams;
+  const query = params.q?.trim() || "";
+
+  const resultPromise = query ? searchRecipes(query) : getAllRecipes();
+
+  return (
+    <>
+      <SearchBar initialQuery={query} />
+      <Suspense key={query} fallback={<RecipeGridLoading />}>
+        <RecipeGrid resultPromise={resultPromise} query={query} />
+      </Suspense>
+    </>
+  );
 }
